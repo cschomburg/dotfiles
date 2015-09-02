f8212613eaf9c240d45b0634f6d5401e073327ec
Constantin Schomburg <me@cschomburg.com>
2015-09-02 23:51:21 +0200

    add daily attic backup service

---
+++ bin/attic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+ATTIC=/usr/local/bin/attic
+
+$ATTIC create --stats -v \
+	~/sync/attic::`date +%Y-%m-%d` \
+	~ \
+	--exclude ~/sync \
+	--exclude ~/backup \
+	--exclude ~/downloads \
+	--exclude ~/jail/downloads \
+	--exclude ~/jail/Downloads \
+	--exclude ~/VirtualBox\ VMs \
+	--exclude ~/.vagrant.d/boxes
+
+$ATTIC prune ~/sync/attic --keep-within=10d --keep-weekly=4 --keep-monthly=-1
