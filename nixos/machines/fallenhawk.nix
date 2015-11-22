87e2f18fde3a19c7bb651319415e90920b0c14d3
Constantin Schomburg <me@cschomburg.com>
2015-11-22 19:25:34 +0100

    nixos/fallenhawk: fail2ban and firewall rules

--- nixos/machines/fallenhawk.nix
+++ nixos/machines/fallenhawk.nix
@@ -5,5 +5,20 @@
     [
       ../profiles/vpn.nix
     ];
+
+  environment.systemPackages = with pkgs; [
+    irssi
+  ];
+
+  services.openssh.startWhenNeeded = false;
+  services.fail2ban.enable = true;
+  services.fail2ban.jails.ssh-iptables = "enabled = true";
+
+  # OpenVPN traffic forwarding
+  networking.firewall.extraCommands = '';
+    iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -o vethlxc2 -j MASQUERADE
+    iptables -A INPUT -i tun+ -j ACCEPT
+    iptables -A FORWARD -i tun+ -j ACCEPT
+  '';
 }
 
