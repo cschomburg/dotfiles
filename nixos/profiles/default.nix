6a549641d54e97dbd4b6d930cf211f94bc3caa6c
Constantin Schomburg <me@cschomburg.com>
2016-11-26 13:36:46 +0100

    Acdcloud attribution, new SSH public key

--- nixos/profiles/default.nix
+++ nixos/profiles/default.nix
@@ -36,7 +36,10 @@
     description = "Constantin Schomburg";
     extraGroups = [ "wheel" "networkmanager" "vboxusers" "docker" ];
     isNormalUser = true;
-    openssh.authorizedKeys.keys = [ "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIqYpmMPdFnkZjmkaOqo7DyFkOf9HPNJBK6nQnEoOSI/CWhYcVGLuHBJTXwovHetcXg/ENZ6SFJJlfoD7KeiWO/KhmTCUwHcBZHTPVQTgKOm4WiTxvcc8uFD38jnWYAn30Phy3xogEm4KMxsE+mI0A//ckq8cu24M2pGGdefpKecDBhocypOOfh8Dg2VWEHZAC+MXpwBawOw7+XP6IoRttfKHIaoh/f8ZryDAHCFRXhI6eCYGH5sULgWqviTyErc0MUVEIyYLrMqOkcTGqG3shfhuTNh1FH6oaWSctQNyAU7bdjq7bSzXkL9E1KznHXo/Jj1Cea6EyGbNz+qh9jZ3b me@cschomburg.com" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDIqYpmMPdFnkZjmkaOqo7DyFkOf9HPNJBK6nQnEoOSI/CWhYcVGLuHBJTXwovHetcXg/ENZ6SFJJlfoD7KeiWO/KhmTCUwHcBZHTPVQTgKOm4WiTxvcc8uFD38jnWYAn30Phy3xogEm4KMxsE+mI0A//ckq8cu24M2pGGdefpKecDBhocypOOfh8Dg2VWEHZAC+MXpwBawOw7+XP6IoRttfKHIaoh/f8ZryDAHCFRXhI6eCYGH5sULgWqviTyErc0MUVEIyYLrMqOkcTGqG3shfhuTNh1FH6oaWSctQNyAU7bdjq7bSzXkL9E1KznHXo/Jj1Cea6EyGbNz+qh9jZ3b me@cschomburg.com"
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDe2ekPNmD3OQFLX/knkGJqguRwCleqIVeY3AH3ATiTgjTxKuQhhVzOGU3vS+5+dywPj4WSmAzmjpekN+umqPUIS0ciQudsU6hnNq4eTutb1sy+EQCY/z99lmJel19TQi2WOa8drbPdrjfZy8AOiPd206gp9HoPHjVN5AaTJvU1NT4Vtt1Bc+7acJdXh6gW8WeYCNonNfxxoV6oKZkXUZHJ5vTd7NKn79CmC9N/Tqc52/r6TmnzBZowTs9xGo//HIB80zH2Qp9/Ebgon1vrpZyOLZ9C1yCYURmNDGKRuiffHi1M6wqlmNuO85atg8OmlZsJmme3LT/2IJxyr2kHKhmt6fbKk0w/cf8hQbs53KOfOEO3wtt38FLvR2hYJOLeep6q/Y3UT4TQVuLtEEgAA1wDbctJRFobOAR3Vp+FfBtN7L5GfjVu5I1CMKlPmryo7pU80HYUEr8GsIWp4NqLpjOpEjojT+tc6+hzBBVg50sAbHnjzvzo0m9WfYg21hyqcTPKIm2zbYy16MZs2d4tyJgXOhaYgXNT0LvnTdK2wNJmF7aYzNoSocoixJnRqesjVKCclJLar/cSr3N3Tf34B/zuDRgfzAMR+9D1sFZ6GGKYRgDACGuA1Kt/X0ld/7Mizf2GS0bXzsJAb/hULtE3FqlyrwiVsaYyhFvE8SN8bCJrrQ== me@cschomburg.com"
+      ];
     uid = 1000;
   };
 
