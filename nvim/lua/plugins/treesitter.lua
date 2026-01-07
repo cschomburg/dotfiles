cf0d2dfb217c8a40bbe857a02b5dc015d59c04ab
Constantin Schomburg <me@cschomburg.com>
2026-01-07 21:21:11 +0100

    updates

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,3 @@
 return function()
-    local configs = require('nvim-treesitter.configs')
-    local parser_configs = require("nvim-treesitter.parsers").get_parser_configs()
 
-    parser_configs.http = {
-        install_info = {
-            url = "https://github.com/NTBBloodbath/tree-sitter-http",
-            files = { "src/parser.c" },
-            branch = "main",
-        },
-    }
-
-    configs.setup {
-        -- ensure_installed = { "php "},
-        highlight = {
-            enable = true,
-        },
-        indent = {
-            enable = true,
-        },
-    }
 end
